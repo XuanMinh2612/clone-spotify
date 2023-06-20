e0320e123bf818eb3f8706516f23443823fc51d0
minh <dangcongxuanminh2612@gmail.com>
2023-06-20 17:38:21 +0700

    test

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./items/header";
 
 function Main() {
-  return <div className="h-screen bg-green-300">Main</div>;
+  return (
+    <div className="h-screen bg-green-300 ">
+      <Header />
+    </div>
+  );
 }
 
 export default Main;
