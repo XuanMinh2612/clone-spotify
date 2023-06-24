9b52d199026cf48e2f79e0adec61ae5b210c9b3b
minh <dangcongxuanminh2612@gmail.com>
2023-06-24 19:16:21 +0700

    .

--- src/components/playSongs/Plays.jsx
+++ src/components/playSongs/Plays.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Plays({ items }) {
   return (
-    <div className="bg-red-400 h-20 w-3/5 absolute bottom-0 flex flex-row justify-between items-center ">
+    <div className="bg-black h-20 w-3/5 absolute bottom-0 flex flex-row justify-between items-center ">
       <div></div>
       <div className="">
         <audio src={items?.hub?.actions?.[1]?.uri} controls className="" />
